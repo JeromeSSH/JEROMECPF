cb7efb9c6f90fd9d77cfc164dd7fc31b32fde6bc
JeromeSSH <hongjeromee@gmail.com>
2024-10-28 14:50:22 +0000

    initial commit

--- pages/CPF Calculator.py
+++ pages/CPF Calculator.py
@@ -718,5 +718,18 @@ def main():
         * MediSave Account: 4% p.a.
     """)
 
+    with st.expander("IMPORTANT NOTICE", expanded=False):
+        st.write("""
+    This web application is a prototype developed for educational purposes only. 
+    The information provided here is NOT intended for real-world usage and should not 
+    be relied upon for making any decisions, especially those related to financial, 
+    legal, or healthcare matters.
+
+    Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. 
+    You assume full responsibility for how you use any generated output.
+
+    Always consult with qualified professionals for accurate and personalized advice.
+    """)
+
 if __name__ == "__main__":
     main()
